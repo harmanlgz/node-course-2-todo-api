3f3d43fa2d704d4330376ef1c8d66dc759d69095
Harman Singh <harman.lgz@gmail.com>
2019-02-18 15:12:46 +0530

    corrected db url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect(process.env.MONGODB_URI || 'https://cloud.mongodb.com/v2/5c38729ff2a30b1794299723#metrics/replicaSet/5c387680cf09a229be557a73/explorer/todoapp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://https://cloud.mongodb.com/v2/5c38729ff2a30b1794299723#metrics/replicaSet/5c387680cf09a229be557a73/explorer/todoapp');
 
 module.exports = {mongoose};
