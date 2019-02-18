42daff0605cae85c073003d73eb36f0d2ad537ed
Harman Singh <harman.lgz@gmail.com>
2019-02-18 15:44:55 +0530

    new mongodb url added

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://cloud.mongodb.com/v2/5c38729ff2a30b1794299723#metrics/replicaSet/5c387680cf09a229be557a73/explorer/todoapp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://dbuser1:dbuser1@harmanlz-shard-00-00-26oe6.mongodb.net:27017,harmanlz-shard-00-01-26oe6.mongodb.net:27017,harmanlz-shard-00-02-26oe6.mongodb.net:27017/test?ssl=true&replicaSet=harmanlz-shard-0&authSource=admin&retryWrites=true');
 
 module.exports = {mongoose};
