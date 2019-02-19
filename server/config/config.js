da0a2528911aaae4b1db69807bb9b06e709b2407
Harman Singh <harman.lgz@gmail.com>
2019-02-19 14:28:29 +0530

    add MONGODB_URI for production env

--- server/config/config.js
+++ server/config/config.js
@@ -6,4 +6,6 @@ if(env === 'development'){
 }else if(env === 'test'){
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}else if(env === 'production'){
+    process.env.MONGODB_URI = 'mongodb+srv://dbuser1:dbuser1@harmanlz-26oe6.mongodb.net/TodoApp?retryWrites=true'
 }
