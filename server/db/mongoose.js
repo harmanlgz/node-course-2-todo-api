4b5db0232e665100d7fa33734cae047f587a1287
Harman Singh <harman.lgz@gmail.com>
2019-02-19 14:22:06 +0530

    setup separate development and test envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-process.env.MONGODB_URI = 'mongodb+srv://dbuser1:dbuser1@harmanlz-26oe6.mongodb.net/TodoApp?retryWrites=true';
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://dbuser1:dbuser1@harmanlz-26oe6.mongodb.net/TodoApp?retryWrites=true');
+// process.env.MONGODB_URI = 'mongodb+srv://dbuser1:dbuser1@harmanlz-26oe6.mongodb.net/TodoApp?retryWrites=true';
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
